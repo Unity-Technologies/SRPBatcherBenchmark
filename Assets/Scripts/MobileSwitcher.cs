254820bd66b9bb36a010c0a792f1536da05d5a85
arnaud <arnaud@unity3d.com>
2019-02-13 16:25:43 +0100

    now profiler is SRPBatcherProfiler and MobileSwitcher GUI added

---
+++ Assets/Scripts/MobileSwitcher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+
+public class MobileSwitcher : MonoBehaviour
+{
+	
+	public SRPBatcherProfiler	m_srpProfiler;
+	
+	void OnGUI()
+	{
+		float offset = 256 + 50;
+		if (GUI.Button(new Rect(32, offset, 150, 150), "Toggle batcher"))
+		{
+			GraphicsSettings.useScriptableRenderPipelineBatching ^= true;
+		}
+ 		if (GUI.Button(new Rect(32 + 150 + 32, offset, 150, 150), "Toggle stats"))
+ 		{
+ 			m_srpProfiler.m_Enable ^= true;
+ 		}
+	}
+	
+}
